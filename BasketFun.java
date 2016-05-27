9b9da9a7bc753a974637ff8fc8bd5d6b0fd1e893
Alice <zhangjiaying2000@hotmail.com>
2016-05-26 23:44:14 -0400

    Update BasketFun.java

--- BasketFun.java
+++ BasketFun.java
@@ -5,48 +5,47 @@ import java.util.*;
 import javax.swing.*;
 import java.io.*;
 import javax.imageio.*;
- 
-/** Purpose: The purpose of this class is to create the panels
-  * used in the levels of the game. This class creates the buttons,
-  * the panels, and it adds the background to the panel.
+import java.awt.event.*;
+
+/** The BasketFun class creates the game screen panel and contains all of the game logic. The game screen will have 5
+  * food buttons that are different depending on the selected level, a pause button to pause the game midway, a
+  * button to reset everything in the basket, and a check button to see if the user has fulfilled the request. These
+  * buttons are created in this class. It will also add the background to the panel.
   * 
-  *  @author Top Of The Stack(C Liu)
-  *  @version 1 05.20.16 Spent 4 hours(Fixing images, testing , etc.)
+  *  @author Top Of The Stack(C Liu) and modified by Top of The Stack (Alice Z)
+  *  @version 2 05.20.16 Spent 7 hours
+  * 
+  * <p>
+  * <b> Instance variables: </b>
+  * <p>
+  * <b> bCol </b> This Color variable is used to store the background colour.
+  * <p>
+  * <b> backName </b> This String is used to store the file name of the background image being used for the level.
+  * <p>
+  * <b> j </> This referene variable is used to point at the JFrame object that is created in the constructor.
   */ 
-//implements ActionListener
-public class BasketFun extends JPanel {
+
+public class BasketFun extends JPanel{
   
   private int levelNum;
   private String backName;
   private Color bCol;
-  /**Purpose: The purpose of this method is to construct the
-    * JFrame, set the size, and add this panel to the JFrame
-    * of Level 1.
-    * It also creates all the buttons, adds the Flow Layout 
-    * to the panel, and allows the panel to be visible.
-   */
-  public void makePanel1()
-  {
-     add(makeButtons("Apple","Click here to drop one apple into the basket!"));
-     add(makeButtons("Orange","Click here to drop one orange into the basket!"));
-    add(makeButtons("Banana","Click here to drop one banana into the basket!"));
-     add(makeButtons("Grape","Click here to drop one grape into the basket!"));
-     add(makeButtons("Watermelon","Click here to drop one watermelon into the basket!"));
-    
-    revalidate();
-    repaint();
-  }
+  JFrame j;
   
-/**Purpose: The purpose of this method is to construct the
-     BasketFun class.
-   */
- 
-   public BasketFun(int levelNum, String bName, Color s) { 
-     super();
-     this.levelNum=levelNum;
-     backName=bName;
-     bCol=s;
-    JFrame j=new JFrame("A Basket Full Of Fun: Level 1");
+  /**The class constructor will create a JPanel that is added to a JFrame that is also created here. The buttons that
+    * all levels have in common: pause, empty, and check, are made and added here as well. The layout used is flow. 
+    * Depending on the level selected, the backgrounds, button creation methods, and themes are different.
+    * @param levelNum This int is used to store which level the user has chosen to play.
+    * @param bName This String is used to store the name of the background file.
+    * @param s This Color is used to store the colour of the background.
+    */
+  public BasketFun(int levelNum, String bName, Color s) { 
+    super();
+    this.levelNum=levelNum;
+    backName=bName;
+    bCol=s;
+    System.out.println(backName);
+    j=new JFrame("A Basket Full Of Fun: Level 1");
     j.setSize(1000,850);
     this.setPreferredSize(new Dimension( 1000,850));
     j.add(this);
@@ -58,83 +57,124 @@ public class BasketFun extends JPanel {
     this.setLayout(f);
     JButton check=new JButton("CHECK!");
     JButton empty=new JButton ("Empty the basket!");
-  
-   add(makeButtons("Pause","Click here to pause the game!"));
-     
+    
+    JButton pause = makeButtons("Pause","Click here to pause the game!");
+    add(pause);
+    pause.addActionListener(new ActionListener(){
+      public void actionPerformed(ActionEvent e)
+      {
+        //stop timer, make things appear
+      }});
+    
     if (levelNum==1)
     {
-    makePanel1();
+      makePanel1();
     }
     else if (levelNum==2)
     {
       makePanel2();
     }
     else{
-      
       makePanel3();
     }
-     add(check);
+    add(check);
     add(empty);
     add(makeButtons("Basket","Clicking the buttons, puts food into this basket!"));
-     revalidate();
-     repaint();
-  }  
-   
-  public void paintComponent( Graphics g)
+    revalidate();
+    repaint();
+  }
+  
+  /**This method will make and add the forest themed buttons to the panel for Level 1.
+    * The parameter for the inner methods is e - a reference variable for ActionEvent.
+    * @throws i IOException This is to prevent crashing when using FileIO.
+    */
+  public void makePanel1()
   {
-     
+    JButton apple =makeButtons("Apple","Click here to drop an apple into the basket!"), orange = makeButtons("Orange","Click here to drop an orange into the basket!");
+    add(apple);
+    apple.addActionListener(new ActionListener(){
+      public void actionPerformed(ActionEvent e)
+      {
+        try
+        {
+          Image test =ImageIO.read (new File ("apple.jpg"));
+          ImageIcon icon=new ImageIcon(test);
+          JLabel label=new JLabel(icon);
+          add(label);
+          revalidate();
+          repaint();
+        }
+        catch(IOException i)
+        {}
+      }});
+    add(orange);
+    orange.addActionListener(new ActionListener(){
+      public void actionPerformed(ActionEvent e)
+      {
+        try
+        {
+          Image test =ImageIO.read (new File ("orange.jpg"));
+          ImageIcon icon=new ImageIcon(test);
+          JLabel label=new JLabel(icon);
+          add(label);
+          revalidate();
+          repaint();
+        }
+        catch(IOException i)
+        {}
+      }});
+    add(makeButtons("Banana","Click here to drop a banana into the basket!"));
+    add(makeButtons("Grape","Click here to drop a grape into the basket!"));
+    add(makeButtons("Watermelon","Click here to drop a watermelon into the basket!"));
+  }
+  
+  
+  /**This method will draw the level backgrounds depending on the level.
+    * @param g This will be used to draw the image of the background onto the panel.
+    * @throws e IOException This is to prevent crashing when using FileIO.
+    */
+  public void paintComponent(Graphics g)
+  {
     BufferedImage b=null;
     super.paintComponent(g);
-    try{
-      b= ImageIO.read(new File (backName+".jpg"));
+    try
+    {
+      b = ImageIO.read(new File (backName+".jpg"));
       g.drawImage(b,0,0,null);
     }
     catch(IOException e){
-      System.out.println("HI");
     }
-   
   }
-   
- 
-   /**Purpose: The purpose of this method is to construct the
-    * JFrame, set the size, and add this panel to the JFrame
-    * of Level 2.
-    * It also creates all the buttons, adds the Flow Layout 
-    * to the panel, and allows the panel to be visible.
-   */
+  
+  /**This method will make and add the underwater themed buttons to the panel for Level 2. */
   public void makePanel2()
   {
-     add(makeButtons("Red","Click here to drop one apple into the basket!"));
-     add(makeButtons("Yellow","Click here to drop one orange into the basket!"));
-     add(makeButtons("Green","Click here to drop one banana into the basket!"));
-     add(makeButtons("Octopus","Click here to drop one grape into the basket!"));
-     add(makeButtons("Crab","Click here to drop one watermelon into the basket!"));
-     revalidate();
-     repaint();
+    add(makeButtons("Red","Click here to drop a red fish into the basket!"));
+    add(makeButtons("Yellow","Click here to drop a yellow fish into the basket!"));
+    add(makeButtons("Green","Click here to drop a green fish into the basket!"));
+    add(makeButtons("Octopus","Click here to drop an octopus into the basket!"));
+    add(makeButtons("Crab","Click here to drop a crab into the basket!"));
   }
   
+  /**This method will make and add the farm themed buttons to the panel for Level 3.*/
   public void makePanel3()
   {
-    add(makeButtons("Tomato","Click here to drop one apple into the basket!"));
-     add(makeButtons("RedA","Click here to drop one orange into the basket!"));
-     add(makeButtons("GreenA","Click here to drop one banana into the basket!"));
-     add(makeButtons("Carrot","Click here to drop one grape into the basket!"));
-     add(makeButtons("Potato","Click here to drop one watermelon into the basket!"));
-     revalidate();
-     repaint();
+    add(makeButtons("Tomato","Click here to drop a tomato into the basket!"));
+    add(makeButtons("RedA","Click here to drop a red apple into the basket!"));
+    add(makeButtons("GreenA","Click here to drop a green apple into the basket!"));
+    add(makeButtons("Carrot","Click here to drop a carrot into the basket!"));
+    add(makeButtons("Potato","Click here to drop a potato into the basket!"));
   }
   
-   
-  /**Purpose: The purpose of this method is to create the 
-    * buttons in the panels. This method adds images to the buttons
-    * in the panels.
+  /**This method creates the buttons based off of the passed in image file's name and the text for the tool tip. The
+    * method then returns the button so that it can be added to the panel.
     * 
-    * @param imageName String passes in the image name
-    * @param text String passes in text for the tool tip
-   */
+    * @param imageName String passed in the for the image file's name.
+    * @param text String passed in text for the tool tip.
+    */
   public JButton makeButtons(String imageName,String text)
   {
-    ImageIcon icon=new ImageIcon(this.getClass().getResource( imageName+".jpg"));
+    ImageIcon icon=new ImageIcon(this.getClass().getResource( imageName + ".jpg"));
     JButton button=new JButton(icon);
     button.setBackground(bCol);
     button.setToolTipText(text);
@@ -147,14 +187,9 @@ public class BasketFun extends JPanel {
 //    
 //  }
   
-  //forest green and farm grass green;new Color(37,177,77)
-  //ocean 0,126,255
-  
-  public static void main(String[] args) { 
-    BasketFun s=new BasketFun (3,"back3", new Color (37,177,77));
-  }
-  
-  
-  
-  
+//  public static void main(String[] args) { 
+//    BasketFun s= new BasketFun (1,"back1", new Color (37,177,77));
+//    //forest green and farm grass green;new Color(37,177,77)
+//    //ocean 0,126,255
+//  }
 }
